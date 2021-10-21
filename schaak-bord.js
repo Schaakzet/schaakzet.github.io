23b9d7004a56af0d1364ddcd2b72be6ef4f79e94
Danny Engelman <danny@engelman.nl>
2021-10-21 14:31:48 +0200

    Init schaak-bord component

--- schaak-bord.js
+++ schaak-bord.js
@@ -1,1 +1,7 @@
+customElements.define("schaak-bord",class extends HTMLElement{
+	connectedCallback(){
+  	let templ = document.querySelector('template[id="SCHAAKBORD_ROB"]').content;
+  	this.append(templ.cloneNode(true));
+  }
+} )
 
