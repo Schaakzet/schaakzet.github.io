04bb5686bc249c6c5dfdc2ebf092366936af292a
Danny Engelman <danny@engelman.nl>
2021-10-21 14:44:20 +0200

    init

--- schaak-bord.js
+++ schaak-bord.js
@@ -1,7 +1,9 @@
-customElements.define("schaak-bord",class extends HTMLElement{
-	connectedCallback(){
-  	let templ = document.querySelector('template[id="SCHAAKBORD_ROB"]').content;
-  	this.append(templ.cloneNode(true));
-  }
-} )
+/*
 
+*/
+customElements.define("schaak-bord", class extends HTMLElement {
+    connectedCallback() {
+        let templ = document.querySelector('template[id="SCHAAKBORD_ROB"]').content;
+        this.append(templ.cloneNode(true));
+    }
+})
