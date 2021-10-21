f882f4fa82e403c99eaba72bfbe360637b45dd47
Danny Engelman <danny@engelman.nl>
2021-10-21 15:26:49 +0200

    2 new Components

--- schaak-bord.js
+++ schaak-bord.js
@@ -3,7 +3,17 @@
 */
 customElements.define("schaak-bord", class extends HTMLElement {
     connectedCallback() {
-        let templ = document.querySelector('template[id="SCHAAKBORD_ROB"]').content;
+        this.create("SCHAAKBORD_ROB");
+    }
+    create(name) {
+        let templ = document.querySelector(`template[id="${name}"]`).content;
         this.append(templ.cloneNode(true));
+        this.board = document.querySelector("#schaakbord");
+    }
+    add(piece, position = "d5") {
+        let schaakstuk = document.createElement("schaak-stuk");
+        schaakstuk.setAttribute("is", piece);
+        schaakstuk.setAttribute("at", position);
+        return this.board.appendChild(schaakstuk);
     }
 })
--- schaak-stuk.js
+++ schaak-stuk.js
@@ -1,1 +1,12 @@
-
+/*
+    <schaak-stuk> Web Component
+*/
+customElements.define(
+    "schaak-stuk",
+    class extends HTMLElement {
+      connectedCallback() {
+        this.innerHTML = `<img src="https://schaakzet.github.io/img/${this.getAttribute("is")}.png">`;
+      }
+    }
+  );
+  
