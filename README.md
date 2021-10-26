4d676dc5fb7a1ec9048e0b4f43e56e00e12bd906
Danny Engelman <danny@engelman.nl>
2021-10-26 16:08:40 +0200

    Upload voorbereiding donderdag

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # schaakzet.github.io
 Roads Technology SchaakZet project
+
+
+Repo: https://github.com/schaakzet/schaakzet.github.io
--- schaak-stuk.js
+++ schaak-stuk.js
@@ -1,10 +1,13 @@
 /*
-    <schaak-stuk> Web Component
+    <schaak-stuk is="wit-paard" at="D5"> Web Component
 */
 customElements.define(
     "schaak-stuk",
     class extends HTMLElement {
       connectedCallback() {
+        this.render();
+      }
+      render(){
         this.innerHTML = `<img src="https://schaakzet.github.io/img/${this.getAttribute("is")}.png">`;
       }
     }
