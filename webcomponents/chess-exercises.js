6698148bb3ef9164f07df99f24604853a9b77dd4
robvdS1986 <r.vanderschaaf@ziggo.nl>
2022-01-26 16:31:38 +0100

    forEach loops funtioneren... morgen if else of...

--- webcomponents/chess-exercises.js
+++ webcomponents/chess-exercises.js
@@ -3,29 +3,17 @@ function showEXselectors() {
   document.getElementById("buttonsExercise").style.visibility = "visible";
 }
 
-
-
-function hideAllExercises_and_Show_One(a, b, c, d, e, f){
-
+function hideAllExercises_and_Show_One(a) {
+  console.log("a=", a);
   const quizNodeList = document.querySelectorAll("chess-exercise");
   document.getElementById("board2exercise").style.display = "none";
 
-  quizNodeList.forEach(
-    function (currentValue, currentIndex) {
-      console.warn(currentIndex, currentValue);
-      currentValue.hidden(true);
-    },
-
-  );
- quizNodeList[a].hidden(false);
- quizNodeList[b].hidden(false);
- quizNodeList[c].hidden(false);
- quizNodeList[d].hidden(false);
- quizNodeList[e].hidden(false);
- quizNodeList[f].hidden(false);
+  quizNodeList.forEach(function (exercise, currentIndex) {
+    console.warn("WAARDE:", currentIndex, exercise);
+    exercise.hidden(true);
+  });
+  quizNodeList[a].hidden(false);
 }
-
-
 
 customElements.define(
   "chess-exercise",
@@ -46,39 +34,47 @@ customElements.define(
       }
     }
 
-    connectedCallback() {
+    connectedCallback(x) {
       console.error("connected");
-      
+
       this.hidden(true);
 
       this.querySelector(`[id]`);
-      
+
       setTimeout(() => {
-        console.error("connected?")
+        console.error("connected?");
         this.setQuiz({});
 
-        
-        console.error(this.shadowRoot.querySelector(`#conclusion`));
-        
+        console.error(this.shadowRoot.querySelector("#buttons"));
 
-        this.getElementById("buttons").onclick = (event) => {
+        this.shadowRoot.getElementById("buttons").onclick = (event) => {
           console.error(event.target.getAttribute("slot")); // dit is jouw input
-          
+
+          const conclusionNodeList = this.querySelectorAll(`[id]`);
+          console.error("is this id", conclusionNodeList);
+          conclusionNodeList.forEach(function (conclusion, currentIndex) {
+            console.warn("concIndex", currentIndex, conclusion);
+            
+            conclusion.onclick = (evt) => {
+              console.error("CONCLUSION!",evt.target);
+            };
+          });
+
           let letter = event.target.getAttribute("slot"); // bepaal de letter
-          this.querySelector(`[id="A"]`).setAttribute("hidden", "true");
-          this.querySelector(`[id="B"]`).setAttribute("hidden", "true");
-          this.querySelector(`[id="C"]`).setAttribute("hidden", "true");
-          this.querySelector(`[id="D"]`).setAttribute("hidden", "true");
-          this.querySelector(`[id="E"]`).setAttribute("hidden", "true");
-          this.querySelector("#" + letter).removeAttribute("hidden");
-          
+
           console.warn(letter);
+          
+          let x = this.querySelector("#" + letter).removeAttribute("hidden");
+
+          conclusionNodeList[x];
+
+          
+        
         };
       });
     }
 
     setQuiz() {
-      
       let chessboard = this.getElement("#board2");
       chessboard.clear();
       chessboard.fen = this.getAttribute("fen");
