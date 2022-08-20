7518a2a3aba3ee92922e061fcee15bddedf0570b
Danny Engelman <danny@engelman.nl>
2022-08-20 11:55:58 +0200

    documented app-base-functions

--- webcomponents/app-base-functions.js
+++ webcomponents/app-base-functions.js
@@ -1,6 +1,13 @@
 // ********************************************************** Generic Helper Functions
+// Generic App(plication) Base Helper Functions that can be used in any project
+
+// ---------------------------------------------------------- isString
+// global isString function: if( isString(param) ) { ... }
+if (window.isString) console.error("conflict: isString() is already defined");
 window.isString = (value) => typeof value === "string" || value instanceof String;
 
-console.todo = function(...args){
-    console.log("%c TODO: ", "background:red;color:yellow", ...args);
-}
+// ---------------------------------------------------------- console.todo
+// extend existing console with a .todo function, coloring the output in red
+console.todo = function (...args) {
+  console.log("%c TODO: ", "background:red;color:yellow", ...args);
+};
