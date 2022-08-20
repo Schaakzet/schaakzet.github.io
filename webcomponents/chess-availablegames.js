d909312edd94cf16958ef12c11ff9aecf9d9154b
Danny Engelman <danny@engelman.nl>
2022-08-20 15:29:11 +0200

    refactored and documented chess-availablegames component

--- webcomponents/chess-availablegames.js
+++ webcomponents/chess-availablegames.js
@@ -1,37 +1,59 @@
 !(function () {
-  // <chess-match> encapsulates <chess-board>
-  // manages players
-  // communicates with server
-  function log(...args) {
-    console.log("%c chess-availablegames ", "background:blue;color:yellow", ...args);
+  const __COMPONENT_NAME__ = "chess-availablegames";
+  /*
+  <chess-availablegames>
+  (development file) seats.html lists available chessmatches with optional WHERE filter
+  each match/line is created with <create-html>
+
+  METHODS:
+    get_availableGames() - internal use only
+    deleteMatch() - used by button in <create-html> code
+
+Notes:
+  manages players
+  communicates with server
+  ! looks like deleteMatch Event is prosessed/logged twice
+  !? missing: method resumeGame() - used by button in <create-html> code
+*/
+  const __AVAILABLEGAMES__ = "AVAILABLEGAMES"; //!! matching name in SQL backend!!
+
+  // custom log colors for this file
+  function log() {
+    console.log(`%c ${__COMPONENT_NAME__} `, "background:blue;color:yellow", ...arguments);
   }
 
-  // ********************************************************** CSS for Full Screen
-  const CSS_Match = /* css */ `:fullscreen {background-color: beige}`;
   // ********************************************************** define <chess-match>
   customElements.define(
-    "chess-availablegames",
+    __COMPONENT_NAME__,
     class extends CHESS.ChessBaseElement {
       connectedCallback() {
         super.connectedCallback();
-        this.render();
-      }
-      // ================================================== render
-      render() {
-        let where = this.getAttribute("where") || "AVAILABLEGAMES";
+        // -------------------------------------------------- render
+        let where = this.getAttribute("where") || __AVAILABLEGAMES__;
+
+        // append/render STYLE and DIV
         this.append(
-          Object.assign(document.createElement("style"), {
-            innerHTML: CSS_Match,
+          this.$createElement({
+            tag: "style",
+            props: {
+              //! does this work?
+              innerHTML: /*css*/ `:fullscreen {background-color: green}`,
+            },
           }),
-          Object.assign(document.createElement("div"), {
-            innerHTML: `<h2>${where}</h2>`,
+          this.$createElement({
+            tag: "div",
+            props: {
+              innerHTML: /* html */ `<h2>${where}</h2>`,
+            },
           })
         );
 
         this.get_availableGames(where);
       }
       // ================================================== deleteMatch
+      //! is deleteMatch used?
       deleteMatch(match_guid) {
+        log("deleteMatch", match_guid);
         window.CHESS.APIRT.deleteMatchByGUID(match_guid);
       }
       // ================================================== get_availableGames
@@ -42,37 +64,63 @@
           body: {
             where,
           },
-          // ------------------------------------------------- callback
+          // ------------------------------------------------- callback after API READ
           callback: ({ rows }) => {
             if (rows.length) {
+              log(rows.length, "rows");
               // --------------------------------------------- create matches DOM elements
-              let matches = rows.map((match_row) => {
-                let { tournament_id, wp_user_white, wp_user_black, player_white, player_black, starttime, endtime, fen, result, match_guid } = match_row;
+              const matchesHTMLArray = rows.map(
+                ({
+                  //!! one match row data:
+                  //tournament_id, //
+                  wp_user_white,
+                  //wp_user_black,
+                  player_white,
+                  //player_black,
+                  //starttime,
+                  //endtime,
+                  //fen,
+                  //result,
+                  match_guid,
+                }) => {
+                  // ------------------------------------------------- callAPI READ
+                  const /* function */ createHTMLButton = (label, eventValue) => {
+                      //!! function INSIDE .map so we can use row data
+                      //! eventValue should be methods on this component?
+                      return (
+                        `<create-html>button` + // create <button>
+                        `|${__COMPONENT_NAME__}:${eventValue}:${match_guid}` + // Event name:value:data
+                        `|${label}` + // button label
+                        `</create-html>`
+                      );
+                    };
 
-                // ------------------------------------------------- callAPI READ
-                let gameButtons = "";
-                if (where == "AVAILABLEGAMES") {
-                  gameButtons = `<create-html>button|chess-availablegames:resumeChessGame:${match_guid}|play Black</create-html>`;
+                  // ------------------------------------------------- return DOM element in map
+                  return this.$createElement({
+                    tag: "div",
+                    props: {
+                      innerHTML:
+                        `white:<b>${player_white}</b> (wp:${wp_user_white}) ${match_guid}` + //
+                        (where === __AVAILABLEGAMES__
+                          ? createHTMLButton("play Black (resume)", "resumeChessGame") //! missing method?
+                          : "") + // no buttons for other tables
+                        createHTMLButton("deleteMatch", "deleteMatch"),
+                    },
+                  }); // return
                 }
-                gameButtons += `<create-html>button|chess-availablegames:deleteMatch:${match_guid}|delete match</create-html>`;
+              ); // rows.map
 
-                // ------------------------------------------------- return DOM element
-                return this.$createElement({
-                  tag: "div",
-                  props: {
-                    innerHTML: `white:<b>${player_white}</b> (wp:${wp_user_white}) ${match_guid} ${gameButtons}`,
-                  },
-                });
-              });
               // ------------------------------------------------- append DOM elements
-              this.append(...matches);
+              this.append(...matchesHTMLArray);
             } else {
+              // no rows.length
               log("Geen matches gevonden in de database");
             }
-          },
-        });
-      }
-    } //class
-  ); //customElements.define("chess-match", class extends CHESS.ChessBaseElement
+          }, // callback
+        }); // callAPI
+      } // get_availableGames
+    } // class
+  ); // customElements.define
+
   // ********************************************************** end IIFE
 })();
