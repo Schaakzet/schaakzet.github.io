efb02badf8aae8bd13e60d73ebc25d98efef13d7
Danny Engelman <danny@engelman.nl>
2022-08-20 16:11:18 +0200

    refactored and documented CHESS.APIRT in chess-base-api-rt.js

--- webcomponents/chess-base-api-rt.js
+++ webcomponents/chess-base-api-rt.js
@@ -1,107 +1,143 @@
-CHESS.APIRT = CHESS.APIRT || {};
+!(function () {
+  /*
+  adds GLOBAL code to GLOBAL CHESS.APIRT Object (API Roads Technology)
+  having it on APIRT makes it easier to (one time) switch to other API endpoints
 
-// add constants
-Object.assign(CHESS.APIRT, {
-  __API_RECORDS__: "//schaakzet.nl/api/crud/index.php/records/",
-  __API_SCHAAKZET__: "//schaakzet.nl/api/chessgame.php/?action=",
-  __API_MATCHES__: "//schaakzet.nl/api/chessgame.php",
-  __API_MATCHMOVES__: "//schaakzet.nl/api/rt/matchmoves.php", // CHESS.__API_MATCHMOVES__
-  __API_MATCHMOVES_EVENTSOURCE__: "//schaakzet.nl/api/rt/matchmoves_eventsource.php",
-  __API_TABLE_MATCHMOVES__: "matchmoves",
-  __API_HEADERS__: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+  CHESS.APIRT.constants
+  CHESS.APIRT.functions
+  deleteMatchByGuid
+  callAPI
+  createMatch
+  undoChessMove //! unused?
+  deleteStartboards //! unused?
+  */
+  const __API__ = "//schaakzet.nl/api/";
+  const __API_CHESSGAME__ = "chessgame.php";
+  const __API_RT_DIRECTORY__ = "rt/";
+  const __API_MATCHMOVES__ = "matchmoves.php";
+  const __API_MATCHMOVES_EVENTSOURCE__ = "matchmoves_eventsource.php";
+  const __API_TABLE_MATCHMOVES__ = "matchmoves";
 
-  // ================================================== deleteMatchByGUID
-  deleteMatchByGUID: (guid) => {
-    fetch(CHESS.APIRT.__API_SCHAAKZET__ + "DELETEMATCH" + "&match_guid=" + guid, {
-      method: "GET",
-      headers: CHESS.APIRT.__API_HEADERS__,
-    });
-  },
-  // ================================================== callAPI
-  callAPI({
-    action,
-    body = {}, // JSON
-    method = "GET",
-    callback = () => console.error("No callback function"),
-  }) {
-    function log(...args) {
-      let label = args.shift();
-      let color = label.includes("fetched") ? "green" : "red";
-      console.log(`%c ${label} `, `background:gold;color:${color}`, ...args);
-    }
-    body.action = action;
-    // -------------------------------------------------- build options
-    let options = {
-      method,
-      //mode: "no-cors",
-      headers: CHESS.APIRT.__API_HEADERS__,
-      body: JSON.stringify(body),
-    };
-    // -------------------------------------------------- build URI
-    let uri = CHESS.APIRT.__API_MATCHES__ + "?c=" + new Date() / 1;
-    if (method == "GET") {
-      uri += "&action=" + body.action;
+  // custom log colors for this file
+  function log(...args) {
+    let label = args.shift(); // take first parameter as label
+    let color = label.includes("fetched") ? "green" : "red"; // color by label
+    console.log(`%c ${label} `, `background:gold;color:${color}`, ...args);
+  }
 
-      //! chessboard.id == match_guid in database
-      if (body.match_guid) uri += "&match_guid=" + body.match_guid;
-      else if (body.id) uri += "&match_guid=" + body.id;
+  // ********************************************************** CHESS.APIRT
+  CHESS.APIRT = CHESS.APIRT || {};
 
-      if (body.where) uri += "&where=" + body.where;
-      if (body.wp_user_white) uri += "&wp_user_white=" + body.wp_user_white;
-      if (body.player_white) uri += "&player_white=" + body.player_white;
-      if (body.wp_user_black) uri += "&wp_user_black=" + body.wp_user_black;
-      if (body.player_black) uri += "&player_black=" + body.player_black;
-      if (body.move) uri += "&move=" + body.move;
-      if (body.fen) uri += "&fen=" + body.fen;
+  // add CHESS.APIRT.constants
+  Object.assign(CHESS.APIRT, {
+    __API_RECORDS__: __API__ + "crud/index.php/records/", //! old CRUD API, before Bart created our own
+    __API_SCHAAKZET__: __API__ + __API_CHESSGAME__ + "/?action=",
+    __API_MATCHES__: __API__ + __API_CHESSGAME__,
+    __API_MATCHMOVES__: __API__ + __API_RT_DIRECTORY__ + __API_MATCHMOVES__, // CHESS.__API_MATCHMOVES__
+    __API_MATCHMOVES_EVENTSOURCE__: __API__ + __API_RT_DIRECTORY__ + __API_MATCHMOVES_EVENTSOURCE__,
+    __API_TABLE_MATCHMOVES__: __API_TABLE_MATCHMOVES__, //! looks likes this is not used any more
+    __API_HEADERS__: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
 
-      delete options.body;
-    }
-    log(`action=${action}`, body.where || body.id || body);
-    // -------------------------------------------------- fetch
-    fetch(uri, options)
-      .then((response) => response.json())
-      .then((json_response) => {
-        console.log("JSON Response:", json_response);
-        let rowcount = json_response.rows.length;
-        if (rowcount == 1) {
-          log(`fetched ${body.action} ${body.where || ""}:`, { ROW: json_response.rows[0] });
-        } else {
-          log(`fetched ${body.action} ${body.where || ""} ${rowcount} rows:`, json_response);
-        }
-        callback(json_response);
-      })
-      .catch((e, r) => {
-        console.error("%c No JSON response!", "background:red;color:white", e);
+    // add CHESS.APIRT.functions
+    // ================================================== deleteMatchByGUID
+    deleteMatchByGUID: (guid) => {
+      fetch(CHESS.APIRT.__API_SCHAAKZET__ + "DELETEMATCH" + "&match_guid=" + guid, {
+        method: "GET",
+        headers: CHESS.APIRT.__API_HEADERS__,
       });
-  },
+      return;
+      //! todo: replace with proper API call
+      CHESS.APIRT.callAPI({
+        action: "DELETEMATCH",
+        body: {
+          match_guid: guid,
+        },
+      });
+    },
+    // ================================================== callAPI
+    callAPI({
+      action,
+      body = {}, // JSON
+      method = "GET",
+      callback = () => console.error("APIRT.callAPI(): No callback function define"),
+    }) {
+      body.action = action;
+      // -------------------------------------------------- build options
+      let options = {
+        method,
+        //mode: "no-cors",
+        headers: CHESS.APIRT.__API_HEADERS__,
+        body: JSON.stringify(body),
+      };
+      // -------------------------------------------------- build URI
+      let uri = CHESS.APIRT.__API_MATCHES__ + "?c=" + new Date() / 1; // with cache buster in URI
+      if (method == "GET") {
+        uri += "&action=" + body.action;
 
-  // ================================================== createMatch
-  createMatch({
-    player_white = "", // player white name
-    player_black = "", // player black name
-    callback,
-  }) {
-    CHESS.APIRT.callAPI({
-      action: "CREATE",
-      body: {
-        player_white: player_white,
-        player_black: player_black,
-      },
+        //! chessboard.id == match_guid in database
+        if (body.match_guid) uri += "&match_guid=" + body.match_guid;
+        else if (body.id) uri += "&match_guid=" + body.id;
+
+        if (body.where) uri += "&where=" + body.where;
+        if (body.wp_user_white) uri += "&wp_user_white=" + body.wp_user_white;
+        if (body.player_white) uri += "&player_white=" + body.player_white;
+        if (body.wp_user_black) uri += "&wp_user_black=" + body.wp_user_black;
+        if (body.player_black) uri += "&player_black=" + body.player_black;
+        if (body.move) uri += "&move=" + body.move;
+        if (body.fen) uri += "&fen=" + body.fen;
+
+        delete options.body;
+      }
+      log(`action=${action}`, body.where || body.id || body);
+      // -------------------------------------------------- fetch
+      fetch(uri, options)
+        .then((response) => response.json())
+        .then((json_response) => {
+          log("JSON Response:", json_response);
+          let rowcount = json_response.rows.length;
+          if (rowcount == 1) {
+            log(`fetched ${body.action} ${body.where || ""}:`, { ROW: json_response.rows[0] });
+          } else {
+            log(`fetched ${body.action} ${body.where || ""} ${rowcount} rows:`, json_response);
+          }
+          //!! execute callback function
+          callback(json_response);
+        })
+        .catch((e, r) => {
+          console.error("%c No JSON response!", "background:red;color:white", e);
+        });
+    },
+
+    // ================================================== createMatch
+    createMatch({
+      player_white = "", // player white name
+      player_black = "", // player black name
       callback,
-    });
-  },
+    }) {
+      CHESS.APIRT.callAPI({
+        action: "CREATE",
+        body: {
+          player_white,
+          player_black,
+        },
+        callback,
+      });
+    },
 
-  // ================================================== undoChessMove
-  undoChessMove({ id, callback }) {
-    let body = { id: this.match_guid };
-    CHESS.APIRT.callAPI("delete", body, callback);
-  },
+    // ================================================== undoChessMove
+    undoChessMove({ id, callback }) {
+      let body = { id: this.match_guid };
+      //! unused?? because callAPI has the old multi parameter syntax, not an Object
+      CHESS.APIRT.callAPI("delete", body, callback);
+    },
 
-  // ================================================== deleteStartboard
-  deleteStartboards({ callback }) {
-    CHESS.APIRT.callAPI("deleteStartboards", {}, callback);
-  },
-});
+    // ================================================== deleteStartboard
+    deleteStartboards({ callback }) {
+      //! unused?? because callAPI has the old multi parameter syntax, not an Object
+      CHESS.APIRT.callAPI("deleteStartboards", {}, callback);
+    },
+  }); // end CHESS.APIRT object
+})(); // end IIFE
